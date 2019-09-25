cc2b3300d9ce603358ffbc86430d5c83616b4743
Michal Widera <michal@widera.com.pl>
2019-09-25 22:07:27 +0200

    Initial
    Initial commit - work in progress

---
+++ eisenstein.py
@@ -0,0 +1,162 @@
+"""Eisenstein Irrational Fractions Type Implemenation - Python 3.x
+   Copyright (c) 2019 Michal Widera
+"""
+
+from decimal import Decimal
+import math
+
+# https://docs.python.org/3.7/reference/datamodel.html
+
+
+class Eisenstein:
+    def __init__(self, a=0, b=0):
+
+        if isinstance(a, int) and isinstance(b, int):
+            self.a = a
+            self.b = b
+        else:
+            raise TypeError("arguments should be a int")
+
+    def __repr__(self):
+        return "(%s, %sw)" % (self.a, self.b)
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            other = Eisenstein(other, 0)
+        return Eisenstein(self.a + other.a, self.b + other.b)
+
+    def __sub__(self, other):
+        if isinstance(other, int):
+            other = Eisenstein(other, 0)
+        return Eisenstein(self.a - other.a, self.b - other.b)
+
+    def __mul__(self, other):
+        # (a+bw)(c+dw)=(ac-bd)+(bc+ad-db)w
+        # https://en.wikipedia.org/wiki/Eisenstein_integer
+        if isinstance(other, int):
+            other = Eisenstein(other, 0)
+        return Eisenstein((self.a * other.a)-(self.b * other.b), (self.b * other.a)+(self.a * other.b)-(self.b * other.b))
+
+    def __abs__(self):
+        # |a+bw|^2 = a*a - a*b + b*b
+        # https://en.wikipedia.org/wiki/Eisenstein_integer
+        return math.sqrt((self.a * self.a) - (self.a * self.b) + (self.b * self.b))
+
+    __rmul__ = __mul__
+    __radd__ = __add__
+
+    # sprawdzic
+    # wolfram alpha : w = ( -1 + i sqrt(3) ) / 2 ; z = ( a + b * w ) * ( a + b * ( w ^ 2 ) )-> z = a^2 - ab + b^2
+    def __mod__(self, other):
+        if isinstance(other, int):
+            other = Eisenstein(other, 0)
+        w = (-1 + math.sqrt(3)) / 2
+        adivb = complex(self.a, self.b * w) / complex(other.a, other.b * w)
+        a = adivb.real
+        b = adivb.imag
+
+        K = Eisenstein(round(a + b / math.sqrt(3)),
+                       round((b * 2) / math.sqrt(3)))
+        print("self = K * other + R : %r = %r * %r + %r" % (self, K, other,
+                                                            self - K * other))
+        return self - K * other
+
+    def gcd(a, b):
+        """Calculate the Greatest Common Divisor of a and b.
+
+        Unless b==0, the result will have the same sign as b (so that when
+        b is divided by it, the result comes out positive).
+        """
+        while b:
+            a, b = b, a % b
+        return a
+
+
+class EisensteinFraction:
+
+    def __init__(self, numerator=0, denominator=1):
+
+        if isinstance(numerator, Eisenstein):
+            self.n = numerator
+        elif isinstance(numerator, int):
+            self.n = Eisenstein(numerator, 0)
+        else:
+            raise TypeError("numerator should be a int or Eisenstein")
+
+        if isinstance(denominator, Eisenstein):
+            self.d = denominator
+        elif isinstance(denominator, int):
+            self.d = Eisenstein(denominator, 0)
+        else:
+            raise TypeError("denominator should be a int or Eisenstein")
+
+    def __repr__(self):
+        return "(%s/%s)" % (self.n, self.d)
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            other = EisensteinFraction(Eisenstein(other, 0), 1)
+        return EisensteinFraction(self.n * other.d + other.n * self.d, self.d * other.d)
+
+    def __sub__(self, other):
+        if isinstance(other, int):
+            other = EisensteinFraction(Eisenstein(other, 0), 1)
+        return EisensteinFraction(self.n * other.d - other.n * self.d, self.d * other.d)
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            other = EisensteinFraction(Eisenstein(other, 0), 1)
+
+        return EisensteinFraction(self.n * other.n, self.d * other.d)
+
+    def __truediv__(self, other):
+        if isinstance(other, int):
+            other = EisensteinFraction(Eisenstein(other, 0), 1)
+        if isinstance(self, int):
+            other = EisensteinFraction(Eisenstein(other, 0), 1)
+        if isinstance(self, Eisenstein):
+            other = EisensteinFraction(other, 1)
+        return self * inverse(other)
+
+    __rmul__ = __mul__
+    __radd__ = __add__
+
+
+def test():
+    E1 = Eisenstein(2, 1)
+    E2 = Eisenstein(22, 4)
+
+    # print(gcd(E1, 2))
+
+    E3 = E1 + E2
+    E4 = E2 - E1
+    E5 = E1 * E2
+    E6 = 2 * E1 * 2
+
+    print(E1, E2, E3, E4, E5, E6)
+
+    print(math.pow(abs(E5), 2))
+
+    EF1 = EisensteinFraction(E1, 1)
+    EF2 = EisensteinFraction(E2, 1)
+
+    print("EF2 + EF1", EF1 + EF2)
+    print("EF2 / EF1", EF2 / EF1)
+
+    print(EF2 * 3)
+
+    print("modulo E2 % 3", E2, 3, E2 % 3)
+    print("modulo 22 % 3", 22, 3, 22 % 3)
+    #print("gcd 22,2:", Eisenstein.gcd(E2, E1))
+
+    print("Success.")
+
+
+def inverse(e: EisensteinFraction):
+    a = e.n.a
+    b = e.n.b
+    return (EisensteinFraction(a - b, a * a - a * b + b * b) - EisensteinFraction(Eisenstein(0, -b), a * a - a * b + b * b)) * EisensteinFraction(e.d, 1)
+
+
+if __name__ == "__main__":
+    test()
---
+++ operations.py
@@ -0,0 +1,106 @@
+"""Time Series Algebra Equations Implemenation - Python 3.x
+   Copyright (c) 2019 Michal Widera
+"""
+
+from fractions import Fraction
+from math import floor, ceil
+A = range(1, 24)
+deltaA = Fraction(1)
+B = list(map(chr, range(ord('a'), ord('z')+1)))
+deltaB = Fraction(1, 2)
+
+
+def sum(A: list, deltaA: Fraction, B: list, deltaB: Fraction):
+
+    result = []
+    deltaC = min(deltaA, deltaB)
+
+    for i in range(0, 20):
+        if deltaC == deltaA:
+            result.append(str(A[i])+B[int(i*deltaA/deltaB)]),
+        else:
+            result.append(str(A[int(i*deltaB/deltaA)])+B[i]),
+    return result, deltaC
+
+
+def diff(C: list, deltaA: Fraction, deltaB: Fraction):
+
+    result = []
+    deltaC = min(deltaA, deltaB)
+
+    for i in range(0, 10):
+        if deltaA > deltaB:
+            result.append(C[int(ceil(i*deltaA/deltaB))])
+        else:
+            result.append(C[i])
+    return result, deltaC
+
+
+def hash(A: list, deltaA: Fraction, B: list, deltaB: Fraction):
+
+    result = []
+    delta = deltaB / (deltaA + deltaB)
+
+    for i in range(0, 20):
+        if floor(i*delta) == floor((i+1)*delta):
+            result.append(B[i-int(floor((i+1)*delta))])
+        else:
+            result.append(A[int(floor(i*delta))])
+
+    deltaC = (deltaA*deltaB)/(deltaA+deltaB)
+    return result, deltaC
+
+
+def dehasheven(C: list, deltaC: Fraction, deltaA: Fraction):
+
+    result = []
+    deltaB = deltaA*deltaC / (deltaA - deltaC)
+
+    for i in range(0, 6):
+        result.append(C[i+int(ceil((i+1)*deltaA/deltaB))])
+    return result, deltaB
+
+
+def dehashodd(C: list, deltaC: Fraction, deltaB: Fraction):
+
+    result = []
+    deltaA = deltaB*deltaC / (deltaB - deltaC)
+
+    for i in range(0, 6):
+        result.append(C[i+int(i*deltaB/deltaA)])
+    return result, deltaA
+
+
+def vdeltaA(n: int): return Fraction(1, 1) + Fraction(n, 20)
+
+
+def vdeltaB(n: int): return Fraction(1, 1) + Fraction(n, 10)
+
+
+def z(i: int): return vdeltaB(i) / (vdeltaA(i) + vdeltaB(i))
+
+
+def vhash():
+
+    result = []
+    for i in range(0, 20):
+        if int(i * z(i)) == int((i + 1) * z(i + 1)):
+            result.append(B[i - int((i) * z(i))])
+        else:
+            result.append(A[int(i * z(i))])
+    return result
+
+
+def main():
+    hash_result, delta_hash = hash(A, deltaA, B, deltaB)
+    sum_result, delta_sum = sum(A, deltaA, B, deltaB)
+    print("Sum:", sum(A, deltaA, B, deltaB))
+    print("Hash:", hash(A, deltaA, B, deltaB))
+    print("Diff:", diff(sum_result, deltaA, deltaB))
+    print("dehasheven:", dehasheven(hash_result, delta_hash, deltaA))
+    print("dehashodd:", dehashodd(hash_result, delta_hash, deltaB))
+    print("vhash", vhash())
+
+
+if __name__ == '__main__':
+    main()
