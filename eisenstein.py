75266a81c4a20a4f43f3f132defda60d838bdf5c
Piotr Kasprzyk <ciri@ciri.pl>
2019-09-27 06:10:34 +0200

    Comment-out not-yet-working code

--- eisenstein.py
+++ eisenstein.py
@@ -79,10 +79,10 @@ class Eisenstein:
 
         Unless b==0, the result will have the same sign as b (so that when
         b is divided by it, the result comes out positive).
-        """
         while b:
             a, b = b, a % b
         return a
+        """
 
 
 class EisensteinFraction:
