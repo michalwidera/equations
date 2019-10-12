9f58ce930b38b34fbbe0802f8b4cd36f66ddd971
Piotr Kasprzyk <ciri@ciri.pl>
2019-10-12 08:14:55 +0200

    Separate slow tests from fast tests

--- test_code.py
+++ test_code.py
@@ -386,11 +386,10 @@ class TestEisensteinFractionTimeSeriesOperations(unittest.TestCase):
                         check_result_add(add_result)
 
 
-fast_test_ls = [
-    TestEisensteinNumbers,
-    TestEisensteinFractionNumbers,
-    TestEisensteinFractionTimeSeriesOperations,
-]
+fast_test_ls = [TestEisensteinNumbers, TestEisensteinFractionNumbers]
+
+
+slow_test_ls = [TestEisensteinFractionTimeSeriesOperations]
 
 
 def check_result_add(Var: list):
@@ -445,6 +444,11 @@ def add_all_fast(suite):
         suite.addTest(unittest.makeSuite(one_test))
 
 
+def add_all_slow(suite):
+    for one_test in slow_test_ls:
+        suite.addTest(unittest.makeSuite(one_test))
+
+
 def summary_status(suite):
     text_test_result = unittest.TextTestRunner().run(suite)
     return not not (text_test_result.failures or text_test_result.errors)
@@ -453,6 +457,7 @@ def summary_status(suite):
 def perform_tests():
     suite = unittest.TestSuite()
     add_all_fast(suite)
+    add_all_slow(suite)
     return summary_status(suite)
 
 
