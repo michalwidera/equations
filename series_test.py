685411a4bb30a28fdb43dc91ca40a1fa861e9e9b
Piotr Kasprzyk <ciri@ciri.pl>
2019-10-15 11:23:13 +0200

    Place time series tests in separate file

---
+++ series_test.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+from eisenstein_fractions import EisensteinFraction
+from eisenstein import get_dot_product
+from eisenstein_operations import hash_a, add
+import data_sets
+import parameters
+
+
+def check_result_add(Var: list):
+
+    prevNum = 1
+    prevAlpha = "a"
+    for (number, alpha) in Var:
+        if number == prevNum or number == prevNum + 1:
+            pass
+        else:
+            print(number, prevNum)
+            raise SystemExit("Add algorithm fails")
+
+        prevNum = number
+
+        if alpha != prevAlpha or alpha != chr(ord(prevAlpha) + 1):
+            pass
+        else:
+            raise SystemExit("Add algorithm fails")
+
+        prevAlpha = alpha
+
+
+def check_result_hash(Var: list):
+
+    alpha = []
+    digit = []
+
+    for item in Var:
+
+        if type(item) is str:
+            alpha.append(item)
+        else:
+            digit.append(item)
+
+    if digit:
+        for index, item in enumerate(digit):
+            if item != data_sets.A[index]:
+                print("Fail A:", item, data_sets.A[index])
+                print("len A:", len(alpha), "len B:", len(digit))
+                raise SystemExit("This algorithm fails A")
+
+        for index, item in enumerate(alpha):
+            if item != data_sets.B[index]:
+                print("Fail B:", item, data_sets.B[index])
+                print("len A:", len(alpha), "len B:", len(digit))
+                raise SystemExit("This algorithm fails B")
+
+
+class TestEisensteinFractionTimeSeriesOperations(unittest.TestCase):
+    def testHashOne(self):
+        deltaA = EisensteinFraction(1, 0)
+        deltaB = EisensteinFraction(1, 0)
+        if get_dot_product(deltaA, deltaB) > 0:
+            hash_result, delta_hash = hash_a(data_sets.A, deltaA, data_sets.B, deltaB)
+            check_result_hash(hash_result)
+        else:
+            SystemExit("dot product =< 0")
+
+    def testHashMatrix(self):
+        TestRange = parameters.cfg_prm.test_range
+
+        for l in range(TestRange):
+            for k in range(TestRange):
+                for j in range(TestRange):
+                    for i in range(TestRange):
+                        deltaA = EisensteinFraction(i + 1, l)
+                        deltaB = EisensteinFraction(j + 1, k)
+                        if get_dot_product(deltaA, deltaB) > 0:
+                            hash_result, delta_hash = hash_a(
+                                data_sets.A, deltaA, data_sets.B, deltaB
+                            )
+                            check_result_hash(hash_result)
+                        else:
+                            pass
+                            # ("SKIP orthogonal", deltaA, deltaB)
+
+    def testAddMatix(self):
+        TestRange = parameters.cfg_prm.test_range
+
+        for l in range(TestRange):
+            for k in range(TestRange):
+                for j in range(TestRange):
+                    for i in range(TestRange):
+                        deltaA = EisensteinFraction(i + 1, l)
+                        deltaB = EisensteinFraction(j + 1, k)
+
+                        add_result, delta_add = add(
+                            data_sets.A, deltaA, data_sets.B, deltaB
+                        )
+                        check_result_add(add_result)
--- test_code.py
+++ test_code.py
@@ -10,12 +10,9 @@ import sys
 import unittest
 from pathlib import Path
 
-from eisenstein_fractions import EisensteinFraction
-from eisenstein_operations import hash_a, add
-from eisenstein import get_dot_product
 from number_test import TestEisensteinNumbers
 from fraction_test import TestEisensteinFractionNumbers
-import data_sets
+from series_test import TestEisensteinFractionTimeSeriesOperations
 import parameters
 
 
@@ -29,101 +26,10 @@ def runningInTravis():
     return False
 
 
-class TestEisensteinFractionTimeSeriesOperations(unittest.TestCase):
-    def testHashOne(self):
-        deltaA = EisensteinFraction(1, 0)
-        deltaB = EisensteinFraction(1, 0)
-        if get_dot_product(deltaA, deltaB) > 0:
-            hash_result, delta_hash = hash_a(data_sets.A, deltaA, data_sets.B, deltaB)
-            check_result_hash(hash_result)
-        else:
-            SystemExit("dot product =< 0")
-
-    def testHashMatrix(self):
-        TestRange = parameters.cfg_prm.test_range
-
-        for l in range(TestRange):
-            for k in range(TestRange):
-                for j in range(TestRange):
-                    for i in range(TestRange):
-                        deltaA = EisensteinFraction(i + 1, l)
-                        deltaB = EisensteinFraction(j + 1, k)
-                        if get_dot_product(deltaA, deltaB) > 0:
-                            hash_result, delta_hash = hash_a(
-                                data_sets.A, deltaA, data_sets.B, deltaB
-                            )
-                            check_result_hash(hash_result)
-                        else:
-                            pass
-                            # ("SKIP orthogonal", deltaA, deltaB)
-
-    def testAddMatix(self):
-        TestRange = parameters.cfg_prm.test_range
-
-        for l in range(TestRange):
-            for k in range(TestRange):
-                for j in range(TestRange):
-                    for i in range(TestRange):
-                        deltaA = EisensteinFraction(i + 1, l)
-                        deltaB = EisensteinFraction(j + 1, k)
-
-                        add_result, delta_add = add(
-                            data_sets.A, deltaA, data_sets.B, deltaB
-                        )
-                        check_result_add(add_result)
-
-
 fast_test_ls = [TestEisensteinNumbers, TestEisensteinFractionNumbers]
 
 
 slow_test_ls = [TestEisensteinFractionTimeSeriesOperations]
-
-
-def check_result_add(Var: list):
-
-    prevNum = 1
-    prevAlpha = "a"
-    for (number, alpha) in Var:
-        if number == prevNum or number == prevNum + 1:
-            pass
-        else:
-            print(number, prevNum)
-            raise SystemExit("Add algorithm fails")
-
-        prevNum = number
-
-        if alpha != prevAlpha or alpha != chr(ord(prevAlpha) + 1):
-            pass
-        else:
-            raise SystemExit("Add algorithm fails")
-
-        prevAlpha = alpha
-
-
-def check_result_hash(Var: list):
-
-    alpha = []
-    digit = []
-
-    for item in Var:
-
-        if type(item) is str:
-            alpha.append(item)
-        else:
-            digit.append(item)
-
-    if digit:
-        for index, item in enumerate(digit):
-            if item != data_sets.A[index]:
-                print("Fail A:", item, data_sets.A[index])
-                print("len A:", len(alpha), "len B:", len(digit))
-                raise SystemExit("This algorithm fails A")
-
-        for index, item in enumerate(alpha):
-            if item != data_sets.B[index]:
-                print("Fail B:", item, data_sets.B[index])
-                print("len A:", len(alpha), "len B:", len(digit))
-                raise SystemExit("This algorithm fails B")
 
 
 def add_all_fast(suite):
