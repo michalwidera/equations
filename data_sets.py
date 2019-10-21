59bf056662b1f09cfbe2347f455d05835a4e5bfe
Michal Widera <michal@widera.com.pl>
2019-10-21 12:50:16 +0200

    Comment update

--- data_sets.py
+++ data_sets.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+"""
+This is data set for testing and validation purposes.
+A = [1,2,3,...]
+B = ['a','b','c',....]
+"""
+
 A = range(1, 50)
 B = list(map(chr, range(ord("a"), ord("z") + 1)))
 B = B + B + B + B
